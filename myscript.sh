c8362f937a992783e23170f9ef8aa675a7be16e7
leegyeongwhan <leekhy02@naver.com>
2023-09-24 20:00:32 +0900

    Update: new post 2023-09-24-20-00-29

---
+++ myscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUTDIR="published"
+LOCAL_GHOST="http://localhost:2368/"
+YMD=$(date +"%Y-%m-%d-%H-%M-%S")
+
+wget -r -nH -P "$OUTDIR" -E -T 2 -np -k "$LOCAL_GHOST"
+git add . -f
+git commit -m "Update: new post $YMD"
+git push origin published
+
