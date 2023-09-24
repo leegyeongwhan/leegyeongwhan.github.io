b73496f4d018aa497c2c3eddb693af9c6ca2f279
leegyeongwhan <leekhy02@naver.com>
2023-09-24 20:07:54 +0900

    Update: new post 2023-09-24-20-07-50

--- myscript.sh
+++ myscript.sh
@@ -4,7 +4,7 @@ OUTDIR="published"
 LOCAL_GHOST="http://localhost:2368/"
 YMD=$(date +"%Y-%m-%d-%H-%M-%S")
 
-wget -r -nH -P "$OUTDIR" -E -T 2 -np -k "$LOCAL_GHOST"
+wget -r -nH -P . -E -T 2 -np -k "$LOCAL_GHOST"
 git add . -f
 git commit -m "Update: new post $YMD"
 git push origin published
