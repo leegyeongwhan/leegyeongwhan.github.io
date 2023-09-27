dee96dfe98b0de4109eddf9223354f92eb50f3bf
leegyeongwhan <leekhy02@naver.com>
2023-09-28 00:19:00 +0900

    Update: new post 2023-09-28-00-18-57

--- myscript.sh
+++ myscript.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
-
-OUTDIR="published"
+OUTDIR="."
 LOCAL_GHOST="http://localhost:2368/"
 YMD=$(date +"%Y-%m-%d-%H-%M-%S")
 
-wget -r -nH -P . -E -T 2 -np -k "$LOCAL_GHOST"
+# published 폴더로 이동
+cd published
+
+# 웹사이트 다운로드
+wget -r -nH -P "$OUTDIR" -E -T 2 -np -k "$LOCAL_GHOST"
+
+# 변경된 파일 스테이징
 git add . -f
+
+# 커밋 메시지 작성
 git commit -m "Update: new post $YMD"
+
+# 변경사항 푸시
 git push origin published
 
